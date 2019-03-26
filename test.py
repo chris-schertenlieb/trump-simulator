0534b5dca8aed972c33fa8f198163d47d192ae89
chris-schertenlieb <schertec@mail.gvsu.edu>
2019-03-26 15:32:23 -0400

    prints JUST the tweet portion of the file. has a weird 'b' at the beginning

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from io import *
 #lines = [line.rstrip('\n') for line in open('test.txt', 'w')]
 #print (lines)
 
-with open('trump_tweets.txt', 'r', encoding='utf-8', errors='replace') as f:
+with open('trump_tweets.txt', 'r', errors='replace') as f:
     for line in f:
-        print(line.encode("utf-8"))
+        #print(line.encode("utf-8"))
+        newLine = line.split('|')
+        if(len(newLine) > 1):
+            print (newLine[1].encode("utf-8"))
 
