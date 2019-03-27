0aa205516fdea79cb5d0fa655ab3dc9dd0a65a55
chris-schertenlieb <schertec@mail.gvsu.edu>
2019-03-26 20:44:46 -0400

    run the code and wait like 20 mins and you get results!

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
 from io import *
+from textgenrnn import textgenrnn
 
-#lines = [line.rstrip('\n') for line in open('test.txt', 'w')]
-#print (lines)
-
-with open('trump_tweets.txt', 'r', errors='replace') as f:
+# this code is for taking tweets out of a
+# file (tweets.txt in this example) and splitting
+# them on a delimiter if necessary (\) and
+# then pastes them into a new file 'newTweets.txt'
+'''tweetList = []
+with open('tweets.txt', 'r', errors='replace') as f:
     for line in f:
         #print(line.encode("utf-8"))
-        newLine = line.split('|')
+
+        #newLine = line.encode('utf-8')
+        #print(newLine)
+        newLine = line.split('\\')
         if(len(newLine) > 1):
-            print (newLine[1].encode("utf-8"))
+            tweetList.append(newLine[1])
+
+with open('newTweets.txt', 'w') as f:
+    for tweet in tweetList:
+        f.write("%s\n" % tweet)'''
+
+# actual code that runs the thingy
+
+textgen = textgenrnn()
+textgen.train_from_file('newTweets.txt', num_epochs=1)
+textgen.generate()
 
